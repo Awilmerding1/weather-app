a6e8ba25f4a77e733f92b0a4d6bbc630e6ae48eb
Annabel Wilmerding <awilmerding@Annabel-Wilmerdings-MB-Pro.local>
2021-01-29 08:46:08 -0800

    adds prompt_for_zip in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,13 @@ class Cli
 
   def welcome
     puts "Welcome to the weather cli app!"
+    self.prompt_for_zip
+  end
+
+  def prompt_for_zip
+    puts "Please enter your zip code"
+    input = gets.strip
+    # do something with the zip code
   end
 
 end

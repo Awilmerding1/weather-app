47c35c80d860274c0cbe28bf9e8c0753864c6620
Annabel Wilmerding <awilmerding@Annabel-Wilmerdings-MB-Pro.local>
2021-01-29 14:37:06 -0800

    adds @@all variable to location

--- lib/api.rb
+++ lib/api.rb
@@ -7,7 +7,7 @@ class Api
       url = "http://api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{@@api_key}"
       response = HTTParty.get(url)
 
-      if response["message"]
+      if response["message"] 
         return false
       end
       weather_hash = {name: response["name"], wind_speed: response["wind"]["speed"], temp: response["main"]["temp"], feels_like: response["main"]["feels_like"], cloud_cover: response["weather"][0]["description"]}
--- lib/location.rb
+++ lib/location.rb
@@ -1,14 +1,21 @@
 class Location
   attr_accessor :name, :wind_speed, :temp, :feels_like, :cloud_cover
 
+  @@all = []
+
   def initialize(hash)
     # call attr_accessor in here
       hash.each do |key, value|
           self.send("#{key}=", value) if self.respond_to?("#{key}=")
       end
+      @@all << self
+      # self.all << self
       # binding.pry
   end
 
+  def self.all
+    @@all
+  end
   # way to check if we have already requested that location
 
 
